7ca180e391c9a1d4ee580ee45baba110fcbc2802
standardSetting <standardSetting@protonmail.com>
2022-01-19 17:25:54 +1100

    Updates

--- src/Shared/ArrowLeft.js
+++
@@ -1,9 +0,0 @@
-function ArrowLeft(props){
-    return (
-       <svg className="w1" viewBox="0 0 32 32" >
-           <path stroke="none" fill="#591F56" d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
-       </svg>
-    )
-   }
-
-export default ArrowLeft;
--- src/Shared/ArrowRight.js
+++
@@ -1,9 +0,0 @@
-function ArrowRight(props) {
-    return (
-        <svg className="w1" viewBox="0 0 32 32">
-            <path stroke="none" fill="#591F56" d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"></path>
-        </svg>
-    )
-}
-
-export default ArrowRight;
